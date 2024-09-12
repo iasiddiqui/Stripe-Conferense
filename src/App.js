de0f6a7dec10bc328238362bc2770c3bb40f8af9
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-09-12 13:10:37 +0530

    some no used data deleted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
-import Login from './components/LogIn'
-import TopEvent from  './components/Topevents'
-import Venues from './components/Venues'
-import './App.css';
+// import Login from './components/LogIn'
+// import TopEvent from  './components/Topevents'
+// import Venues from './components/Venues'
 const App=()=>(
   <>
    <BrowserRouter>
      <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
+      {/* <Route path="/login" element={<Login/>}></Route>
       <Route path="/top" element={<TopEvent/>}></Route>
-      <Route path="/venue" element={<Venues/>}></Route>
+      <Route path="/venue" element={<Venues/>}></Route> */}
      </Routes>
    </BrowserRouter>
   </>
